c9b884d6dbf86c783dbd3716e3713ff8569a880a
Oriol Martinez <ori.martinez.grau@gmail.com>
2026-04-14 20:10:37 +0200

    Preguntes quiz

---
+++ app.js
@@ -0,0 +1,15 @@
+//** PREGUNTES DEL QUIZ */
+const quizData = {
+    A: [ // CATEGORIA A: JOCS OLÍMPICS
+        { q: "Qui té el rècord de més medalles d'or olímpiques?", a: ["Michael Phelps", "Usain Bolt", "Larisa Latynina"], c: 0 },
+        { q: "A quina ciutat es van celebrar els Jocs de 1992?", a: ["Seül", "Barcelona", "Atenes"], c: 1 },
+        { q: "Cada quants anys es celebren els Jocs Olímpics d'estiu?", a: ["2 anys", "4 anys", "6 anys"], c: 1 },
+        { q: "Quin d'aquests països ha guanyat més medalles totals?", a: ["Xina", "Rússia", "Estats Units"], c: 2 },
+        { q: "On es van inventar els Jocs Olímpics originals?", a: ["Grècia", "Itàlia", "Egipte"], c: 0 },
+        { q: "Quin atleta té el rècord mundial de 100m i 200m?", a: ["Tyson Gay", "Yohan Blake", "Usain Bolt"], c: 2 },
+        { q: "Quants anells formen la bandera olímpica?", a: ["5", "6", "4"], c: 0 },
+        { q: "Quin esport es juga amb una raqueta i un volant?", a: ["Tennis de taula", "Bàdminton", "Squash"], c: 1 },
+        { q: "Quina ciutat acollirà els Jocs Olímpics de 2024?", a: ["Los Angeles", "Tòquio", "París"], c: 2 },
+        { q: "Quin metall forma la medalla del primer classificat?", a: ["Plata", "Or", "Bronze"], c: 1 }
+    ],
+}
